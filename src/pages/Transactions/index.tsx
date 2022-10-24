33b4288f7517b22fc62c08ae1012e192cb0813de
webster ribeiro <webster96ribeiro@gmail.com>
2022-10-24 15:57:56 -0300

    Componente SearchForm concluido

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,41 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+
+export function Transactions(){
+    return (
+        <div>
+            <Header />
+            <Summary />
+
+            <TransactionsContainer>
+                <SearchForm />
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighLight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>12/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hamburguer</td>
+                            <td>
+                                <PriceHighLight variant="outcome">
+                                        R$ 57,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>10/04/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
+        </div>
+    );
+}
